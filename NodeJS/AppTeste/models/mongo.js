62913cf6d228caa36ff14d89c73c20be568279e8
fabriciomaruta <fabricio.maruta@gmail.com>
2019-05-20 16:29:38 -0300

    Created user DB

--- NodeJS/AppTeste/models/mongo.js
+++ NodeJS/AppTeste/models/mongo.js
@@ -1,11 +1,19 @@
 var mongoose = require("mongoose");
-conn1 = mongoose.createConnection('mongodb://localhost:27017/alunosDB', {useNewUrlParser: true});
+conn1 = mongoose.createConnection('mongodb://localhost:27017/forumDB', {useNewUrlParser: true});
 var Schema = mongoose.Schema;
-var alunoSchema = new Schema({
-    "ra": String,
-    "nome": String,
-    "curso": String,
-    "cp":String,
-    "cr":String
+var userSchema = new Schema({
+    "name": String,
+    "login": String,
+    "password": String,
+    "nickname":String,
 });
-module.exports = conn1.model('alunos', alunoSchema);
+
+var postSchema = new Schema({
+    "name": String,
+    "content": String,
+    "owner": String,
+    "comments":Array,
+});
+
+module.exports = conn1.model('post', postSchema);
+module.exports = conn1.model('usuario', userSchema);
